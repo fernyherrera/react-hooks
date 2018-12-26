7c98bddd1794d3a572f2269a988b2f8d7a9c7a47
Fernando <fherrera85@gmail.com>
2018-12-25 22:14:35 -0800

    refactrored app component to use hooks

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-class App extends React.Component {
-  state = { resource : 'posts'}
-  render () {
-    return (
+const App = () => {
+  const [resource, setResource] = useState('posts');
+
+  return (
+    <div>
       <div>
-        <div>
-          <button onClick={() => this.setState({resource: 'posts'})} className="ui button primary">Posts</button>
-          <button onClick={() => this.setState({resource: 'todos'})} className="ui button primary">To Dos</button>
-        </div>
-        {this.state.resource}
+        <button onClick={() => setResource('posts')} className="ui button primary">Posts</button>
+        <button onClick={() => setResource('todos')} className="ui button primary">To Dos</button>
       </div>
-    );
-  };
+      {resource}
+    </div>
+  );
 }
 
 export default App;
